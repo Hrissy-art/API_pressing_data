9e4d4ae043949d798ba3a039f16a5b425d1e1d08
Hristina <hristinastankova89@gmail.com>
2024-01-04 23:16:14 +0100

    District and town table

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Country;
+use App\Entity\District;
 use App\Entity\Material;
 use App\Entity\Product;
+use App\Entity\Town;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -65,5 +68,33 @@ class AppFixtures extends Fixture
         $manager->persist($substance);
     }
     
+    $countries = [];
+
+    for ($i = 0; $i < 30; $i++){
+
+        $country = new Country;
+        $country-> setName ( $faker ->realTextBetween(3,10));
+
+        $manager->persist($country);
+        $countries[] = $country;
+    }
+
+    $districts = [];
+    for ($i = 0; $i < 30; $i++){
+        $district = new District;
+        $district-> setName ( $faker ->realTextBetween(3,10));
+        $manager->persist($district);
+        $districts[] = $district;
+    }
+
+    $towns = [];
+    for ($i = 0; $i < 30; $i++){
+        $town = new Town;
+        $town-> setName ( $faker ->realTextBetween(3,10));
+        $town-> setZipCode ( $faker ->randomNumber(3,10));
+        $manager->persist($town);
+        $towns[] = $town;
+    }
+
     $manager->flush();  }  }
 
---
+++ src/Entity/Town.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TownRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TownRepository::class)]
+class Town
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $zip_code = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(?string $zip_code): static
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+}
