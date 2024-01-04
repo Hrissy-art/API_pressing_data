b4fcc844a6ff68935a2a3d02188cf87085e4605d
Hristina <hristinastankova89@gmail.com>
2024-01-04 22:19:17 +0100

    creating tables Category, Product and Material and adding first fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,68 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Material;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
 
-        $manager->flush();
+       // Catégory
+       $mainCategories = [
+        "Hommes" => [
+            "Chemises" => ["Chemise en coton", "Chemise à carreaux", "Chemise formelle"],
+            "Pantalons" => ["Jean", "Pantalon chino", "Pantalon de costume"],
+            "Pulls" => ["Pull en laine", "Sweat à capuche", "Pull col roulé"],
+        ],
+        "Femmes" => [
+            "Robes" => ["Robe d'été", "Robe de soirée", "Robe maxi"],
+            "Jupes" => ["Jupe crayon", "Jupe plissée", "Jupe longue"],
+            "Ensembles" => ["Ensemble jupe et haut", "Ensemble pantalon et top", "Ensemble robe et veste"],
+        ],
+        "Enfants" => [
+            "T-shirts" => ["T-shirt imprimé", "T-shirt à manches longues", "T-shirt basique"],
+            "Shorts" => ["Short en jean", "Short de sport", "Short en coton"],
+            "Pyjamas" => ["Pyjama deux pièces", "Chemise de nuit", "Combinaison pyjama"],
+        ],
+    ]; 
+    
+    foreach($mainCategories as $mainCategoryLabel => $subCategories){
+        $mainCategory = new Category();
+        $mainCategory->setCategoryName($mainCategoryLabel);
+        $manager->persist($mainCategory);
+        
+        foreach($subCategories as $subCategoryLabel => $products){
+            $subCategory = new Category();
+            $subCategory->setCategoryName($subCategoryLabel);
+            $manager->persist($subCategory);
+            
+            foreach($products as $productName){
+                $product = new Product();
+                $product
+                    ->setProductName($productName)
+                    ->setPrice($faker->randomFloat(2));
+                $manager->persist($product);
+            }
+            $mainCategory->addChild($subCategory);
+        }
+        $manager->persist($mainCategory);
     }
-}
+
+    $substances =["Soie", "Coton", "Lin", "Polyester", "Cuir", "Autres"];
+
+    foreach ($substances as $substanceName) {
+        $substance = new Material();
+        $substance->setName($substanceName)
+                 ->setCoeff($faker->randomFloat(1, 1, 2));       
+        $manager->persist($substance);
+    }
+    
+    $manager->flush();  }  }
+
---
+++ src/Entity/Material.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MaterialRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MaterialRepository::class)]
+class Material
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $coeff = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCoeff(): ?float
+    {
+        return $this->coeff;
+    }
+
+    public function setCoeff(?float $coeff): static
+    {
+        $this->coeff = $coeff;
+
+        return $this;
+    }
+}
